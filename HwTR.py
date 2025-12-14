78cc4902f0d41bce8615f83ba4cd696777981623
Clive <cliveclayirawan2005@gmail.com>
2025-12-14 22:12:40 +0700

    update HwTR model

--- HwTR.py
+++ HwTR.py
@@ -5,73 +5,80 @@ from tensorflow.keras import layers, Model
 from tensorflow.keras import backend as F
 
 
+import numpy as np
+import cv2
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from tensorflow.keras import backend as F
+
+
 class HwTR:
-    def __init__(self, img_w, img_h, max_text_length, num_classes, letters, verbose=0):
+    def __init__(self, img_w, img_h, max_text_length, num_classes, letters):
         self.IMG_W = img_w
         self.IMG_H = img_h
         self.MAX_TEXT_LENGTH = max_text_length
         self.num_classes = num_classes
         self.letters = letters
-        self.verbose = verbose
+
         self.training_model, self.inference_model = self._build_models()
 
     def _build_models(self):
 
-        input_data = tf.keras.layers.Input(
+        input_data = layers.Input(
             name='input', shape=(self.IMG_W, self.IMG_H, 1), dtype='float32'
         )
 
-        # VGG layers
-        x = tf.keras.layers.Conv2D(64, (3,3), padding='same', kernel_initializer='he_normal')(input_data)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.MaxPooling2D((2,2))(x)
+        # CNN (VGG)
+        x = layers.Conv2D(64, (3,3), padding='same', kernel_initializer='he_normal')(input_data)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.MaxPooling2D((2,2))(x)
 
-        x = tf.keras.layers.Conv2D(128, (3,3), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.MaxPooling2D((2,2))(x)
+        x = layers.Conv2D(128, (3,3), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.MaxPooling2D((2,2))(x)
 
-        x = tf.keras.layers.Conv2D(256, (3,3), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.Conv2D(256, (3,3), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.MaxPooling2D((1,2))(x)
+        x = layers.Conv2D(256, (3,3), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(256, (3,3), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.MaxPooling2D((1,2))(x)
 
-        x = tf.keras.layers.Conv2D(512, (3,3), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.Conv2D(512, (3,3), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        x = tf.keras.layers.MaxPooling2D((1,2))(x)
+        x = layers.Conv2D(512, (3,3), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(512, (3,3), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.MaxPooling2D((1,2))(x)
 
-        x = tf.keras.layers.MaxPooling2D((2,1))(x)
+        x = layers.MaxPooling2D((2,1))(x)
 
-        x = tf.keras.layers.Conv2D(512, (2,2), padding='same', kernel_initializer='he_normal')(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
+        x = layers.Conv2D(512, (2,2), padding='same', kernel_initializer='he_normal')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
 
-        # BiLSTM
-        x = tf.keras.layers.Reshape((16, 2048))(x)
-        x = tf.keras.layers.Dense(64, activation='relu', kernel_initializer='he_normal')(x)
+        # LSTM
+        x = layers.Reshape((16, 2048))(x)
+        x = layers.Dense(64, activation='relu', kernel_initializer='he_normal')(x)
 
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True))(x)
-        x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True))(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+        x = layers.Bidirectional(layers.LSTM(256, return_sequences=True))(x)
+        x = layers.Bidirectional(layers.LSTM(256, return_sequences=True))(x)
+        x = layers.BatchNormalization()(x)
 
-        y_pred = tf.keras.layers.Dense(self.num_classes, kernel_initializer='he_normal')(x)
-        y_pred = tf.keras.layers.Activation('softmax', name='softmax')(y_pred)
+        y_pred = layers.Dense(self.num_classes, kernel_initializer='he_normal')(x)
+        y_pred = layers.Activation('softmax', name='softmax')(y_pred)
+
+        # CTC inputs
+        labels = layers.Input(name="the_labels", shape=[self.MAX_TEXT_LENGTH], dtype="int32")
+        input_length = layers.Input(name="input_length", shape=[1], dtype="int32")
+        label_length = layers.Input(name="label_length", shape=[1], dtype="int32")
 
         # CTC
-        labels = tf.keras.layers.Input(name="the_labels", shape=[self.MAX_TEXT_LENGTH], dtype="int32")
-        input_length = tf.keras.layers.Input(name="input_length", shape=[1], dtype="int32")
-        label_length = tf.keras.layers.Input(name="label_length", shape=[1], dtype="int32")
-
-        # CTC
-        loss_out = tf.keras.layers.Lambda(
+        loss_out = layers.Lambda(
             lambda args: F.ctc_batch_cost(args[1], args[0], args[2], args[3]),
             name="ctc"
         )([y_pred, labels, input_length, label_length])
@@ -124,9 +131,9 @@ class HwTR:
     
         img = img.astype(np.float32) / 255.0
     
-        img = img.T                        # Transpose
-        img = np.expand_dims(img, axis=-1) # (128, 64, 1)
-        img = np.expand_dims(img, axis=0)  # (1, 128, 64, 1)
+        img = img.T                                
+        img = np.expand_dims(img, axis=-1)          
+        img = np.expand_dims(img, axis=0)           
     
         return img
 
@@ -148,8 +155,7 @@ class HwTR:
 
     def load(self, checkpoint):
         self.inference_model.load_weights(checkpoint)
-        if self.verbose:
-            print(f"Weights loaded: {checkpoint}")
+        print(f"✔ Weights loaded: {checkpoint}")
 
     def predict(self, img_tensor):
         preds = self.inference_model.predict(img_tensor)
